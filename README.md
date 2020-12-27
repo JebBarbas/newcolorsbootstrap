8789ba7da0cb82d92b824d1320aac532c250f531
Enrique Barboza <enriquebarbozajeb@gmail.com>
2020-12-26 23:05:45 -0800

    v1.3.0 - 12 new colors added (now 16) - opacity style added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # New Colors Bootstrap
-Add 4 new colors to Bootstrap 4 and new components.
+Add 16 new colors to Bootstrap 4 and new components and styles.
 
 ## ALERT
 These are experimental stylesheets I made while was learning SASS. I don't know if they are compatible with all browsers and I'd be very happy if you send me feedback.
@@ -19,17 +19,25 @@ Don't forget to add the .js file too
 <script src="https://cdn.jsdelivr.net/gh/jebbarbas/newcolorsbootstrap/ncb.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/jebbarbas/newcolorsbootstrap/ncb.min.js"></script>
 ~~~
+Or you can add: Bootstrap CSS ,Bootstrap Bundle JS, Ncb CSS and Ncb JS with one line
+~~~
+<script src="https://cdn.jsdelivr.net/gh/jebbarbas/newcolorsbootstrap/ncb.min.js" class="add-full-kit"></script>
+~~~
 You can put the &lt;script&gt; in the &lt;head&gt; or in the &lt;body&gt;, it doesn't matter 'cause I use the event 'DOMContentLoaded' to apply the JS.
 
 ## How To Use - Quick Start
-You need first to have Bootstrap 4, this stylesheet doesn't contains Boostrap 4 styles, only new colors for them.
-Once you have Bootstrap 4, use it normally, but you can use the colors: 'magic','juice','ocean' and 'cake', put them when normally you put 'primary' or 'success'
+You need first to have Bootstrap 4 (or 5-beta), this stylesheet doesn't contains Boostrap 4 styles, only new colors for them.
+Once you have Bootstrap, use it normally, but you can use the colors:  'magic', 'juice', 'ocean', 'cake',
+'brick', 'blood', 'mustard', 'militar', 'lapislazuli', 'grass', 'leaf', 'coral', 'love', 'clay', 'chocolate'
+and 'elegant'.
+And then put them when normally you put 'primary' or 'success'
 
 ## Example
 ~~~
 <button class="btn btn-magic">Magic Button</button>
 <div class="alert alert-juice">Juice Alert</div>
 <button class="btn btn-outline-ocean btn-sm">Small Outline Ocean Button</button>
+<span class="text-blood">Some text in blood color</span>
 ~~~
 
 ## New Things
@@ -37,11 +45,25 @@ Once you have Bootstrap 4, use it normally, but you can use the colors: 'magic',
 - 'juice' color.
 - 'ocean' color.
 - 'cake' color.
+- 'brick' color.
+- 'blood' color.
+- 'mustard' color.
+- 'militar' color.
+- 'lapislazuli' color.
+- 'grass' color.
+- 'leaf' color.
+- 'coral' color.
+- 'love' color.
+- 'clay' color.
+- 'chocolate' color.
+- 'elegant' color.
 - 'Callout' component.
 - 'Sidebar' component.
 - 'Circle' component.
 - 'Circle-State' component.
 - 'Login' component.
+- 'Icon Margin' style.
+- 'Opacity' style.
 
 ## Note
 I added also the SASS source code. Lov U All.
--- ncb.js
+++ ncb.js
@@ -1,28 +1,51 @@
-/* Comprueba si el script contiene la clase add-bootstrap-css, si lo tiene, añade bootstrap y ncb */
-if(document.querySelector('script.add-bootstrap-css')){
-    /* Se crea el <div> para los links */
-    let _div_ = document.createElement('div');
-    _div_.innerHTML = `<!-- code inscrusted By ncb.js (bootstrap 4.5.2 and newcolorsbootstrap) -->`;
+const add_full_kit = ()=>{
+    /* Comprueba si el script contiene la clase add-bootstrap-css, si lo tiene, añade bootstrap y ncb */
+    if(document.querySelector('script.add-full-kit')){
+        /* En esta variable se guarda el comentario que irá en los divs */
+        let com = `<!-- Code inscrusted By NewColorsBootstrap (Bootstrap 5.0.0-beta1 and NewColorsBootstrap 1.3.0) -->`;
 
-    /* Se crea el <link> de bootstrap */
-    let _link_bs_ = document.createElement('link');
-    _link_bs_.setAttribute('rel','stylesheet');
-    _link_bs_.setAttribute('href','https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
-    _link_bs_.setAttribute('crossorigin','annonymus');
+        /* Se crea el <div> para los links */
+        let _div_links_ = document.createElement('div');
+        _div_links_.innerHTML = com;
 
-    /* Se crea el <link> de ncb */
-    let _link_ncb_ = document.createElement('link');
-    _link_ncb_.setAttribute('rel','stylesheet');
-    _link_ncb_.setAttribute('href','https://cdn.jsdelivr.net/gh/jebbarbas/newcolorsbootstrap@v1.2.2/ncb.min.css');
-    _link_ncb_.setAttribute('crossorigin','annonymus');
+        /* Se crea el <div> para los scripts */
+        let _div_scripts_ = document.createElement('div');
+        _div_scripts_.innerHTML = com;
 
-    /* Se meten los <link> en el <div> */
-    _div_.append(_link_bs_);
-    _div_.append(_link_ncb_);
+        /* Se crea el <link> de bootstrap */
+        let _link_bs_ = document.createElement('link');
+        _link_bs_.setAttribute('href','https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css');
+        _link_bs_.setAttribute('rel','stylesheet');
+        _link_bs_.setAttribute('integrity','sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1');
+        _link_bs_.setAttribute('crossorigin','annonymus');
 
-    /* Se mete el <div> en el <head> */
-    document.head.append(_div_);
-}
+        /* Se crea el <link> de ncb */
+        let _link_ncb_ = document.createElement('link');
+        _link_ncb_.setAttribute('href','https://cdn.jsdelivr.net/gh/jebbarbas/newcolorsbootstrap@v1.3.0/ncb.min.css');
+        _link_ncb_.setAttribute('rel','stylesheet');
+        _link_ncb_.setAttribute('crossorigin','annonymus');
+
+        /* Se crea el <script> de bootstrap_bundle */
+        let _script_bs_ = document.createElement('script');
+        _script_bs_.setAttribute('src','https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js');
+        _script_bs_.setAttribute('integrity','sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW');
+        _script_bs_.setAttribute('crossorigin','annonymus');
+
+        /* Se meten los <link> en el <div> */
+        _div_links_.append(_link_bs_);
+        _div_links_.append(_link_ncb_);
+
+        /* Se mete el <script> en el <div> */
+        _div_scripts_.append(_script_bs_);
+
+        /* Se mete el <div>:links en el <head> */
+        document.head.append(_div_links_);
+
+        /* Se mete el <div>:scripts en el <body> */
+        document.body.append(_div_scripts_);
+    }
+};
+
 /* Esta funcion se encarga de mostrar y ocultar el sidebar */
 const ncbToggleSidebar = () => {
     let sidebar = document.getElementsByClassName("sidebar")[0];
@@ -41,6 +64,8 @@ const ncbToggleSidebar = () => {
 };
 /* Esta funcion es para cargar el JS una vez cargado el DOM */
 const ncbChargeJS = () => {
+    add_full_kit();
+
     let sidebar = document.getElementsByClassName("sidebar")[0];
     let sidebar_toggler = document.getElementsByClassName("sidebar-toggler")[0];
     let sidebar_toggler_toggled = document.getElementsByClassName("sidebar-toggler-toggled")[0];
